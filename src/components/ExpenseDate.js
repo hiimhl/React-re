f65117db8ebccff8bce4bac642a354d3476378ee
hlin <gywjd5126@gmail.com>
2022-05-28 15:23:43 +0900

    make component

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" }); //2자리로 출력
+  const year = props.date.getFullYear();
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
+//App 데이터를 Expense라는 이름으로 새로운 컴포넌트로 만들기.
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>Date : May 25th 2022</div>
+      <div>
+        <ExpenseDate date={props.date} />
+      </div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">Amount : $294.67</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
