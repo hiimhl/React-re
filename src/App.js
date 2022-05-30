d45f5d3e260eb0f5f1e69977d0aefe0a497d8fc5
hlin <gywjd5126@gmail.com>
2022-05-30 22:36:03 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -31,7 +32,7 @@ function App() {
 
   return (
     <div>
-      <h1>run start!</h1>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
